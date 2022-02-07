cad2e1bcfed3eab74d8960fe34f5a8d0739b0119
Colin Leong <--unset>
2022-02-07 12:30:50 -0500

    CDL: adding some quotes to a bash script

--- bloom_parse.sh
+++ bloom_parse.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for book in $1/*
+for book in "$1"/*
 do
-    echo $book
-    curfile="$(basename "$book")"
+    echo "$book"
+    # curfile="$(basename "$book")"
+    # echo "curfile $curfile"
     python3 book_parser.py --source "$book" --out "out"
 done
