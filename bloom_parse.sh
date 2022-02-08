7e766b5b7e9f81e0aaf1e06a9bf9f4d4a3cc3ed9
Colin Leong <--unset>
2022-02-08 15:33:20 -0500

    CDL: add a line to make the 'out' directory in the parsing script

--- bloom_parse.sh
+++ bloom_parse.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# TODO: rewrite this whole bash script using Python pathlib within book_parser.py. We just need to iterate over subfolders of a folder, we can do that in Python and add tqdm progress bar. 
 
+# make the out directory, -p so it won't crash if the directory exists already. Also doable in Python. 
+mkdir -p "out"
 for book in "$1"/*
 do
     echo "$book"
