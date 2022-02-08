78b596df3c4541bf7fa16733aed2ef0a04f11c75
Colin Leong <--unset>
2022-02-08 15:36:35 -0500

    CDL: a bit of file path rework, and some reformatting

--- book_parser.py
+++ book_parser.py
@@ -6,96 +6,111 @@ from os.path import isfile, join
 import argparse
 import pandas as pd
 from bs4 import BeautifulSoup
+import pathlib
+
 
 def sorted_book(source):
     book_text = []
     book_sorted = []
-    
+
     mydir = source
-    
+    # print(f"source {source}")
+
     htmfile = [f for f in listdir(mydir) if "htm" in f]
-    with open(os.path.join(mydir, htmfile[0])) as file:
+    # print(f"htmfile {htmfile}")
+    htmfile_path = os.path.join(mydir, htmfile[0])
+    # print(f"thingamajig: {htmfile_path}")
+    with open(htmfile_path) as file:
         page = file.read()
-    
-    soup = BeautifulSoup(page, 'html.parser')
-    
-    divs = soup.findAll('div')
-    
+
+    soup = BeautifulSoup(page, "html.parser")
+
+    divs = soup.findAll("div")
+
     i = 0
     BookText = {}
-    
+
     for div in divs:
-        
-        if div.has_attr('class') and 'bloom-translationGroup' in div.get('class'):
+
+        if div.has_attr("class") and "bloom-translationGroup" in div.get("class"):
             entry = {}
-            subdivs = div.findAll('div')
-            
+            subdivs = div.findAll("div")
+
             for subdiv in subdivs:
-                
-                if (subdiv.has_attr('lang') and 
-                        subdiv.get('lang') != '*' and 
-                        subdiv.get('lang') != ''):
-                    
-                    texts = subdiv.findAll('p')
-                    
-                    book_text = ''
+
+                if (
+                    subdiv.has_attr("lang")
+                    and subdiv.get("lang") != "*"
+                    and subdiv.get("lang") != ""
+                ):
+
+                    texts = subdiv.findAll("p")
+
+                    book_text = ""
                     for text in texts:
-                        if book_text != '':
-                            book_text += '\n' + text.get_text()
+                        if book_text != "":
+                            book_text += "\n" + text.get_text()
                         else:
                             book_text = text.get_text()
-                    entry[subdiv.get('lang')] = book_text
-                    
+                    entry[subdiv.get("lang")] = book_text
+
             if len(entry.keys()) > 0:
                 BookText[i] = entry
-                    
+
             i += 1
 
     return BookText
 
+
 def metadata(source):
     mpath = os.path.join(source, "meta.json")
-    
+
     with open(mpath) as metadata:
         metadata = json.load(metadata)
-    
+
     return metadata
+
 
 def output(source, metadata, book_content, outdir):
     metadata.update(book_content)
-   
-    key = metadata['downloadSource']
-    split_key = key.split('/')
-    filename = split_key[1] + '.json'
+
+    key = metadata["downloadSource"]
+    split_key = key.split("/")
+    filename = split_key[1] + ".json"
 
     with open(os.path.join(outdir, filename), "w") as f:
         metadata_full = json.dump(
-                metadata,f, indent="  ",
-                check_circular=True, allow_nan=True,
-                cls=None, separators=None
-                )
-    
+            metadata,
+            f,
+            indent="  ",
+            check_circular=True,
+            allow_nan=True,
+            cls=None,
+            separators=None,
+        )
+
     return metadata_full
 
+
 def main():
-    parser = argparse.ArgumentParser(
-            description="Preprocess Bloom books"
-            )
+    parser = argparse.ArgumentParser(description="Preprocess Bloom books")
 
     parser.add_argument(
-            "--source",dest='source',
-            help="directory containing the input bloom book files"
-            )
-    parser.add_argument("--out", dest='out',
-            help="output directory for parsed book files"
-            )
+        "--source",
+        dest="source",
+        help="directory containing the input files for one bloom book",
+    )
+    parser.add_argument(
+        "--out", dest="out", help="output directory for parsed book files"
+    )
 
     args = parser.parse_args()
+    # print(f"args.source {args.source}")
 
     book_content = sorted_book(args.source)
     metadata_fin = metadata(args.source)
     output_fin = output(args.source, metadata_fin, book_content, args.out)
 
+
 if __name__ == "__main__":
     main()
-
