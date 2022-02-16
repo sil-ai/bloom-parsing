73acfffcfd77ed73ddcb56c093c494590049cafa
Colin Leong <--unset>
2022-02-16 16:14:30 -0500

    CDL: various updates to book_parser.py including iterating over bloom books, bloom_parse.sh removed

--- book_parser.py
+++ book_parser.py
@@ -6,24 +6,36 @@ from os.path import isfile, join
 import argparse
 import pandas as pd
 from bs4 import BeautifulSoup
-import pathlib
+from pathlib import Path
+
+# TODO: track process in clearML
 
 
-def sorted_book(source):
-    book_text = []
-    book_sorted = []
-
-    mydir = source
+def parse_htmfiles(source_dir):
     # print(f"source {source}")
 
-    htmfile = [f for f in listdir(mydir) if "htm" in f]
-    # print(f"htmfile {htmfile}")
-    htmfile_path = os.path.join(mydir, htmfile[0])
+    htmfiles = [f for f in listdir(source_dir) if "htm" in f]
+    print(f"htmfile {htmfiles}")
+
+    book_texts = []
+    for htmfile in htmfiles:
+        htmfile_path = os.path.join(source_dir, htmfile)
+        book_text = parse_text_from_one_htmfile(htmfile_path)
+        book_texts.append(book_text)
+
+    return book_texts
+
+
+def parse_text_from_one_htmfile(htmfile_path):
+
     # print(f"thingamajig: {htmfile_path}")
     with open(htmfile_path) as file:
         page = file.read()
 
     soup = BeautifulSoup(page, "html.parser")
+
+    booktitle = soup.find("title")
+    print(f"book title: {booktitle.text}")
 
     divs = soup.findAll("div")
 
@@ -31,19 +43,20 @@ def sorted_book(source):
     BookText = {}
 
     for div in divs:
+        if div.has_attr("data-book") and "topic" in div.get("data-book"):
+            print("found the topic div")
+            htmltopic = div.text.strip()
 
         if div.has_attr("class") and "bloom-translationGroup" in div.get("class"):
             entry = {}
             subdivs = div.findAll("div")
 
             for subdiv in subdivs:
-
                 if (
                     subdiv.has_attr("lang")
                     and subdiv.get("lang") != "*"
                     and subdiv.get("lang") != ""
                 ):
-
                     texts = subdiv.findAll("p")
 
                     book_text = ""
@@ -62,8 +75,7 @@ def sorted_book(source):
     return BookText
 
 
-def metadata(source):
-    mpath = os.path.join(source, "meta.json")
+def parse_metadata(mpath):
 
     with open(mpath) as metadata:
         metadata = json.load(metadata)
@@ -71,12 +83,13 @@ def metadata(source):
     return metadata
 
 
-def output(source, metadata, book_content, outdir):
+def output(metadata, book_content, outdir):
     metadata.update(book_content)
 
     key = metadata["downloadSource"]
     split_key = key.split("/")
     filename = split_key[1] + ".json"
+    print(f"output to {filename}")
 
     with open(os.path.join(outdir, filename), "w") as f:
         metadata_full = json.dump(
@@ -98,7 +111,7 @@ def main():
     parser.add_argument(
         "--source",
         dest="source",
-        help="directory containing the input files for one bloom book",
+        help="directory containing the input files for all the bloom books",
     )
     parser.add_argument(
         "--out", dest="out", help="output directory for parsed book files"
@@ -107,9 +120,22 @@ def main():
     args = parser.parse_args()
     # print(f"args.source {args.source}")
 
-    book_content = sorted_book(args.source)
-    metadata_fin = metadata(args.source)
-    output_fin = output(args.source, metadata_fin, book_content, args.out)
+    # book_texts = parse_htmfiles(args.source)
+    source_path = Path(args.source)
+    for book_folder in source_path.iterdir():
+        if book_folder.is_dir():
+
+            htmfiles = list(book_folder.glob("*.htm"))
+            mpath = book_folder / "meta.json"
+            if mpath.exists():
+                metadata_fin = parse_metadata(mpath)
+                print(htmfiles)
+                for htmfile in htmfiles:
+                    if metadata_fin["title"] in htmfile.name:
+                        book_text = parse_text_from_one_htmfile(htmfile)
+                        # exit()
+
+                        output(metadata_fin, book_text, args.out)
 
 
 if __name__ == "__main__":
